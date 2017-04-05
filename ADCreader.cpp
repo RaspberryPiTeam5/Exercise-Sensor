d2121de460ce1eceb357ff3c35d6448453230b90
Rohizzle <Rohizzle@users.noreply.github.com>
2017-04-05 18:27:35 +0100

    Updating for 3 channels

--- ADCreader.cpp
+++ ADCreader.cpp
@@ -27,23 +27,63 @@ int main(void)
 {
     mcp3008Spi a2d("/dev/spidev0.0", SPI_MODE_0, 1000000, 8);
     int i = 20;
-        int a2dVal = 0;
-    int a2dChannel = 0;
-        unsigned char data[3];
+ //channel 0
+        int a2dVal0 = 0;
+    int a2dChannel0 = 0;
+        unsigned char data0[3];
+ //channel 1
+    int a2dVal1 = 0;
+    int a2dChannel1 = 0;
+        unsigned char data1[3];
+ //channel 2
+ int a2dVal2 = 0;
+    int a2dChannel2 = 0;
+        unsigned char data2[3];
  
     while(i > 0)
     {
-        data[0] = 1;  //  first byte transmitted -> start bit
-        data[1] = 0b10000000 |( ((a2dChannel & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
-        data[2] = 0; // third byte transmitted....don't care
+     //channel 0
+        data0[0] = 1;  //  first byte transmitted -> start bit
+        data0[1] = 0b10000000 |( ((a2dChannel0 & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
+        data0[2] = 0; // third byte transmitted....don't care
+     
+     //channel 1
+        data1[0] = 1;  //  first byte transmitted -> start bit
+        data1[1] = 0b10010000 |( ((a2dChannel1 & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=0,D1=0,D0=1)
+        data1[2] = 0; // third byte transmitted....don't care
+    
+     
+      //channel 2
+        data1[0] = 1;  //  first byte transmitted -> start bit
+        data1[1] = 0b10100000 |( ((a2dChannel2 & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=0,D1=1,D0=0)
+        data1[2] = 0; // third byte transmitted....don't care
+     
+     //channel 0
+        a2d.spiWriteRead(data0, sizeof(data0) );
  
-        a2d.spiWriteRead(data, sizeof(data) );
+        a2dVal0 = 0;
+                a2dVal0 = (data0[1]<< 8) & 0b1100000000; //merge data[1] & data[2] to get result
+                a2dVal0 |=  (data0[2] & 0xff);
+     
+      //channel 1
+        a2d.spiWriteRead(data1, sizeof(data1) );
  
-        a2dVal = 0;
-                a2dVal = (data[1]<< 8) & 0b1100000000; //merge data[1] & data[2] to get result
-                a2dVal |=  (data[2] & 0xff);
+        a2dVal1 = 0;
+                a2dVal1 = (data1[1]<< 8) & 0b1100100000; //merge data[1] & data[2] to get result
+                a2dVal1 |=  (data1[2] & 0xff);
+     
+       //channel 1
+        a2d.spiWriteRead(data2, sizeof(data2) );
+ 
+        a2dVal2 = 0;
+                a2dVal2 = (data2[1]<< 8) & 0b1101000000; //merge data[1] & data[2] to get result
+                a2dVal2 |=  (data2[2] & 0xff);
+     
+     
         sleep(1);
-        cout << "The Result is: " << a2dVal << endl;
+        cout << "Temperature is: " << a2dVal0 << endl;
+        cout << "Temperature is: " << a2dVal1 << endl;
+        cout << "Temperature is: " << a2dVal2 << endl;
         i--;
     }
     return 0;
