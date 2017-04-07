2aa8d397e1e835b0c954ee0bad1c62b11aa04df7
Rohizzle <Rohizzle@users.noreply.github.com>
2017-04-07 23:30:20 +0100

    Adding buttons and knob

--- window.h
+++ window.h
@@ -8,6 +8,9 @@
 #include <QBoxLayout>
 #include <QLabel>
 #include "ADCreader.h"
+#include <QPushButton>
+#include <QPen>
+
 
 // class definition 'Window'                                                                                                                   
 class Window : public QWidget
@@ -21,29 +24,50 @@ public:
         ~Window();
 
         void timerEvent( QTimerEvent * );
+                        public slots:
+
+          int tempnorm();
+          int temp0();
+          int sweaty();
+          int dry();
+          void setGain(double gain);
+
 
 
 // internal variables for the window class                                                                                                     
 private:
+        QwtKnob *knob;
 
         QwtPlot      *plot0,*plot1,*plot2;
 
-        QwtPlotCurve *curve0,*curve1,*curve2;
+        QwtPlotCurve *curve0,*curve00,*curve1,*curve2,*curve22;
+
+        QPushButton*button0,*button00,*button2,*button22;
 
         QLabel*Label;
 
         // layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html                                                        
-        QVBoxLayout  *vLayout;  // vertical layout  
+          QVBoxLayout  *vLayout;  // vertical layout                          
+        QVBoxLayout*vLayout1;// vertical layout                             
+        QHBoxLayout*hLayout;// horizontal layout
 
   static const int plotDataSize = 100;
 
         // data arrays for the plot                                                                                                            
-        double xData[plotDataSize];
+         double xData[plotDataSize];
         double yData0[plotDataSize];
-        //double x1Data[plotDataSize];                                                                                                         
-        double yData1[plotDataSize];
-       // double x2Data[plotDataSize];                                                                                                         
+        double yData00[plotDataSize];                                      \
+
+        double yData1[plotDataSize];                                       \
+                                                                           \
         double yData2[plotDataSize];
+        double yData22[plotDataSize];
+        
+        double gain;
+        int normtemp;
+        int moistlev;
+        ADCreader *adcreader;
+
 
         ADCreader *adcreader;
 
