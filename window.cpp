e232eebfee737d54962fd8cfa666837af458c9d6
Rohizzle <Rohizzle@users.noreply.github.com>
2017-04-07 16:13:35 +0100

    Removing

--- window.cpp
+++ window.cpp
@@ -34,11 +34,10 @@ Window::Window()
         curve1->attach(plot1);
         curve2->attach(plot2);
 
-        plot0->setAxisTitle(QwtPlot::xBottom, "Time (x10ms)");
         plot0->setAxisTitle(QwtPlot::yLeft, "Temperature (°C)");
-        plot1->setAxisTitle(QwtPlot::xBottom, "Time (x10ms)");
+      
         plot1->setAxisTitle(QwtPlot::yLeft, "Voltage (mv)");
-        plot2->setAxisTitle(QwtPlot::xBottom, "Time (x10ms)");
+      ;
         plot2->setAxisTitle(QwtPlot::yLeft, "Moisture (units)");
 
         plot0->replot();
@@ -79,21 +78,21 @@ void Window::timerEvent( QTimerEvent * )
         memmove( yData0, yData0+1, (plotDataSize-1) * sizeof(double) );
 	yData0[plotDataSize-1] = inVal0;
         curve0->setSamples(xData, yData0, plotDataSize);
-        plot0->setAxisTitle(QwtPlot::xBottom, "Time(x10ms)");
+        
         plot0->setAxisTitle(QwtPlot::yLeft, "Temperature (°C)");
         plot0->replot();
 
 memmove( yData1, yData1+1, (plotDataSize-1) * sizeof(double) );
         yData1[plotDataSize-1] = inVal1;
         curve1->setSamples(xData, yData1, plotDataSize);
-        plot1->setAxisTitle(QwtPlot::xBottom, "Time(x10ms)");
+        
         plot1->setAxisTitle(QwtPlot::yLeft, "Voltage (mv)");
         plot1->replot();
 
 memmove( yData2, yData2+1, (plotDataSize-1) * sizeof(double) );
         yData2[plotDataSize-1] = inVal2;
         curve2->setSamples(xData, yData2, plotDataSize);
-        plot2->setAxisTitle(QwtPlot::xBottom, "Time(x10ms)");
+       
         plot2->setAxisTitle(QwtPlot::yLeft, "Moisture (units)");
         plot2->replot();
 
